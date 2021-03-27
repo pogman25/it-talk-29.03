251e8fab01c64e54e10585a68cdf4f249a4e4a6d
Alexandr Pogorelov <sapogi21@yandex.ru>
2021-03-27 20:46:10 +0300

    more HOC examples

--- src/HOC/counterHOC/counterHOC.jsx
+++ src/HOC/counterHOC/counterHOC.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import ClimbingBoxLoader from 'react-spinners/ClimbingBoxLoader';
+
+import { HocContainerStyled, ButtonStyled } from './styled';
 
 function counterHOC(WrappedComponent) {
   return class extends Component {
@@ -7,21 +10,42 @@ function counterHOC(WrappedComponent) {
 
       this.state = {
         counter: 0,
+        showSpinner: false,
       };
+
       this.increase = this.increase.bind(this);
+      this.showMeSpinner = this.showMeSpinner.bind(this);
     }
 
     increase() {
       this.setState(({ counter }) => ({ counter: counter + 1 }));
     }
 
+    showMeSpinner() {
+      this.setState(({ showSpinner }) => ({ showSpinner: !showSpinner }));
+    }
+
     render() {
-      const { counter } = this.state;
+      const { counter, showSpinner } = this.state;
       return (
-        <div onClick={this.increase}>
-          <WrappedComponent />
-          <p>{`You click ${counter} times`}</p>
-        </div>
+        <HocContainerStyled>
+          {showSpinner ? (
+            <HocContainerStyled>
+              <ClimbingBoxLoader />
+              <ButtonStyled onClick={this.showMeSpinner}>
+                Back to counter
+              </ButtonStyled>
+            </HocContainerStyled>
+          ) : (
+            <div onClick={this.increase}>
+              <WrappedComponent />
+              <p>{`You click ${counter} times`}</p>
+              <ButtonStyled onClick={this.showMeSpinner}>
+                Show me Spinner
+              </ButtonStyled>
+            </div>
+          )}
+        </HocContainerStyled>
       );
     }
   };
---
+++ src/HOC/counterHOC/styled.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const HocContainerStyled = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin-bottom: 1em;
+`;
+
+export const ButtonStyled = styled.button`
+  position: relative;
+  z-index: 1;
+`;
